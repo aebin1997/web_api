c7b84a1239188bb2288b16652c9f0698aa1994bb
KwonMoose <scrt4364@gmail.com>
2022-10-19 15:27:42 +0900

    launch 설정 수정
    목록 응답 클래스 데이터 처리 가이드

--- TodoApi/Models/Response/TodoListResponse.cs
+++ TodoApi/Models/Response/TodoListResponse.cs
@@ -7,7 +7,13 @@ public class TodoListResponse
 {
     public long TotalCount { get; set; }
 
-    public List<TodoListItem> List { get; set; } = null!;
+    public List<TodoListItem> List { get; set; }
+
+    public TodoListResponse()
+    {
+        Console.WriteLine("TodoListResponse 생성자가 실행되었다.");
+        // 서비스에서 반환 받은 데이터를 해당 위치에서 데이터를 입력할 수 있도록 수정한다.
+    }
 }
 
 public class TodoListItem
